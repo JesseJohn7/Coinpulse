17985252349bc24e2f139b417b2034c95e59479e
JesseJohn7 <jessejohn260@gmail.com>
2026-01-03 11:00:26 +0100

    header and image path

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import Image from 'next/image'
 const page = () => {
   return (
-  <div>
-    
-  </div>
+  <main className='main-container'>
+    <section className='home-grid'>
+      <div id='coin-overview'>
+        <div className='header pt-2'>
+          <Image src='https://assets.coingecko.com/coins/images/1/large/bitcoin.png' alt='Bitcoin' width={56} height={56} />
+          <div className='info'>
+            <p className='name'>Bitcoin / BTC</p>
+            <h1>$89,113.00</h1>
+          </div>
+        </div>
+      </div>
+
+      <p>Trending Coins</p>
+    </section>
+
+    <section className='w-full mt-7 space-y-4'>
+      <p>Categories</p>
+    </section>
+  </main>
   )
 }
 
